f713fa74fc33f0554f15ce0620b802c1c506ee05
Maksat-Kydyrov <maksat.developerkg1@gmail.com>
2025-06-05 14:01:03 +0600

    fixbag/settings.py

--- config/config/settings.py
+++ config/config/settings.py
@@ -13,10 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
-
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
-
+DEBUG = config("DEBUG", cast=bool)
+ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.split(",")])
 
 # Application definition
 
